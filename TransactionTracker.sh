72fb86e37cfb9ea6c403f9d6f8acfe469547d72c
Kim Kiogora <kimkiogora@gmail.com>
2015-05-12 18:37:15 +0300

    Create TransactionTracker.sh

---
+++ TransactionTracker.sh
@@ -0,0 +1,9 @@
+#@uthor <kimkiogora@gmail.com>
+log_file=/data/sample_info.log-20150325.gz
+key_word_A='RequestID'
+key_word_B='2015'
+result=`zgrep -ae $key_word_B $log_file  | grep $key_word_A | awk '{print $12}' | awk -F'|' '{print $1}'|sort|unique`
+for trxID in $result;
+do
+echo "TransactionID $trxID count is" `zgrep -ae $key_word_B $log_file | grep -i $key_word_A| grep -i $trxID | grep -vc "Finished processing request with ID"`
+done
