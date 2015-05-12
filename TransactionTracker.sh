6bdb32faff8e7c6adf0eadaba3621c50e7c548e7
Kim Kiogora <kimkiogora@gmail.com>
2015-05-12 18:39:54 +0300

    Update TransactionTracker.sh

--- TransactionTracker.sh
+++ TransactionTracker.sh
@@ -6,5 +6,5 @@ key_word_M='Finished processing request with ID'
 result=`zgrep -ae $key_word_B $log_file  | grep $key_word_A | awk '{print $12}' | awk -F'|' '{print $1}'|sort|unique`
 for trxID in $result;
 do
-echo "TransactionID $trxID count is" `zgrep -ae $key_word_B $log_file | grep -i $key_word_A| grep -i $trxID | grep -vc $key_word_M`
+echo "TransactionID $trxID Processing Count is" `zgrep -ae $key_word_B $log_file | grep -i $key_word_A| grep -i $trxID | grep -vc $key_word_M`
 done
