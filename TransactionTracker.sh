c5015e1e15e392038be0d01a5dced89c76dfb48a
Kim Kiogora <kimkiogora@gmail.com>
2015-05-13 11:17:22 +0300

    Update TransactionTracker.sh

--- TransactionTracker.sh
+++ TransactionTracker.sh
@@ -3,7 +3,7 @@ log_file=/data/sample_info.log-20150325.gz
 key_word_A='RequestID'
 key_word_B='2015'
 key_word_M='Finished processing request with ID'
-result=`zgrep -ae $key_word_B $log_file  | grep $key_word_A | awk '{print $12}' | awk -F'|' '{print $1}'|sort|unique`
+result=`zgrep -ae $key_word_B $log_file  | grep $key_word_A | awk '{print $12}' | awk -F'|' '{print $1}'|sort|uniq`
 for trxID in $result;
 do
 echo "TransactionID $trxID Processing Count is" `zgrep -ae $key_word_B $log_file | grep -i $key_word_A| grep -i $trxID | grep -vc $key_word_M`
