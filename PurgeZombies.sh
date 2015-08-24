b6a5397291446c555b4995e64ec766c1c38a7896
Kim Kiogora <kimkiogora@gmail.com>
2015-08-24 11:29:18 +0300

    Create PurgeZombies.sh

---
+++ PurgeZombies.sh
@@ -0,0 +1,25 @@
+zombie_suspect='some-script.php'
+count=`ps aux | grep -c $zombie_suspect`
+
+echo "$count process(es) found"
+
+if [ $count -gt 1 ];then
+echo "Purging time !!"
+pids=`ps aux | grep $zombie_suspect | awk '{print $2}'| uniq | sort`
+for pid in $pids
+do
+echo "Purges $pid"
+kill -9 $pid
+sleep 0.05
+done
+
+echo "Revalidating ..."
+sleep 1
+#recap
+count=`ps aux | grep -c c2b-poller.php`
+if [ $count -eq 0 ];then
+echo "Purge completed $count process(es) found"
+else
+echo "Purge failed ! $count process(es) still exist"
+fi
+fi
