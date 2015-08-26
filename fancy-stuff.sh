c53839c34aed458adc6fbd6a4a9885e8c41a90ef
Kim Kiogora <kimkiogora@gmail.com>
2015-08-26 18:05:13 +0300

    Update fancy-stuff.sh

--- fancy-stuff.sh
+++ fancy-stuff.sh
@@ -3,3 +3,6 @@ ls -l | awk '{if ($5 > $your_size_here) print $9}'
 
 #Indepth search
 du -a . | awk '$1*512 < 2000 {print $2} '
+
+#Substring example
+"tail -n10000 Somelog.log  | grep "SomeID : "|awk '{print $7}' | cut -c1-5 | sort |uniq"
