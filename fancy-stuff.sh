a9d9b1cb7e6dafea48b5732eb3544eb2531c459f
Kim Kiogora <kimkiogora@gmail.com>
2015-08-28 13:05:12 +0300

    Update fancy-stuff.sh

--- fancy-stuff.sh
+++ fancy-stuff.sh
@@ -5,4 +5,10 @@ ls -l | awk '{if ($5 > $your_size_here) print $9}'
 du -a . | awk '$1*512 < 2000 {print $2} '
 
 #Substring example
-"tail -n10000 Somelog.log  | grep "SomeID : "|awk '{print $7}' | cut -c1-5 | sort |uniq"
+tail -n10000 Somelog.log  | grep "SomeID : "|awk '{print $7}' | cut -c1-5 | sort |uniq"
+
+#list directories only
+ls -l | egrep '^d'
+
+#List files only
+ls -l | egrep -v ‘^d’
