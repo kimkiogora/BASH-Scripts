9d61b62094487443726d0b8f688c4d452b27c0b3
Kim Kiogora <kimkiogora@gmail.com>
2016-07-21 16:38:18 +0300

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -2,3 +2,11 @@
 ps aux | awk '{print $11}' | sort | uniq -c | sort -nk1 | tail -n5
 
 ps aux | awk '{print $1}' | sort | uniq -c | sort -nk1 | tail -n5
+
+
+load=`uptime | awk '{print $8,$9,$10}' | sed -e "s/,//g" | tr " " "\n"`
+
+for i in $load
+do
+echo "Load is $i"
+done
